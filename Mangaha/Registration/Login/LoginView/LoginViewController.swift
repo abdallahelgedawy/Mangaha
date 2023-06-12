1a17068f2e9f9ebde92f3d6d193ae222a29c5b48
abdallahelgedawy <abdallahelgedawy1@gmail.com>
2023-06-13 02:28:10 +0300

    authentication implementation

--- Mangaha/Registration/Login/LoginView/LoginViewController.swift
+++ Mangaha/Registration/Login/LoginView/LoginViewController.swift
@@ -6,41 +6,63 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Lottie
 
 
 class LoginViewController: UIViewController {
-   // let animationView = LottieAnimationView()
+    @IBOutlet weak var passwordTF: UITextField!
+    let animationView = LottieAnimationView()
+    @IBOutlet weak var emailTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-      //  setupAnimation()
+        setupAnimation()
         loginBtn.layer.cornerRadius = 10
         
     }
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func myLoginBtn(_ sender: UIButton) {
-  
+        guard let email = emailTF.text else {return}
+        guard let password = passwordTF.text else {return}
+        Auth.auth().signIn(withEmail: email, password: password){ authresult , error in
+            if let error = error{
+                let alertController = UIAlertController(title: "Alert", message: error.localizedDescription, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+                }
+                
+                alertController.addAction(okAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }else{
+                let homeVc = HomeViewController(nibName: "HomeViewController", bundle: nil)
+                self.navigationController?.pushViewController(homeVc, animated: true)
+            }
+            
+            
+        }
     }
     
     @IBAction func noAccountBtn(_ sender: Any) {
         let signUpVc = SignUpViewController(nibName: "SignUpViewController", bundle: nil)
-                navigationController?.pushViewController(signUpVc, animated: true)
+        navigationController?.pushViewController(signUpVc, animated: true)
     }
-    /*   func setupAnimation(){
-        animationView.animation = LottieAnimation.named("allsports")
+    func setupAnimation(){
+        animationView.animation = LottieAnimation.named("signin")
         animationView.contentMode = .scaleAspectFill
         animationView.loopMode = .loop
         animationView.play()
         animationView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(animationView)
-            // Center the animation view horizontally
-            NSLayoutConstraint.activate([
-                animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            ])
-            
-            // Center the animation view vertically
-            NSLayoutConstraint.activate([
-                animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            ])
-  */
-
+        // Center the animation view horizontally
+        NSLayoutConstraint.activate([
+            animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+        // Center the animation view vertically
+        NSLayoutConstraint.activate([
+            animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+        
+        
+    }
 }
--- Mangaha/Registration/SignUp/SignUpView/SignUpViewController.swift
+++ Mangaha/Registration/SignUp/SignUpView/SignUpViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         registerBtn.layer.cornerRadius = 10
@@ -16,6 +19,33 @@ class SignUpViewController: UIViewController {
 
     @IBOutlet weak var registerBtn: UIButton!
     
-
+    @IBAction func signUpBtn(_ sender: UIButton) {
+        guard let email = emailTF.text else{return}
+        guard let password = passwordTF.text else{return}
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                let alertController = UIAlertController(title: "Alert", message: error.localizedDescription, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+                }
+                
+                alertController.addAction(okAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            
+            }else{
+                let alertController = UIAlertController(title: "Alert", message: "Registration Sucessfully", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+                }
+                
+                alertController.addAction(okAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            
+                let loginVc = LoginViewController(nibName: "LoginViewController", bundle: nil)
+                self.navigationController?.pushViewController(loginVc, animated: true)
+            }
+        }
+    }
+    
 
 }
