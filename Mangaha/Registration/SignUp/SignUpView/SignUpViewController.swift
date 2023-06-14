c86f98d819686e1150f2898e67d319c6f62bc04a
abdallahelgedawy <abdallahelgedawy1@gmail.com>
2023-06-14 10:58:06 +0300

    store customer id

--- Mangaha/Registration/SignUp/SignUpView/SignUpViewController.swift
+++ Mangaha/Registration/SignUp/SignUpView/SignUpViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 import FirebaseAuth
-
+import FirebaseFirestore
 class SignUpViewController: UIViewController {
-
+    let db = Firestore.firestore()
+    let signUpViewModel = SignUpViewModel()
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var usernameTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         registerBtn.layer.cornerRadius = 10
@@ -33,11 +35,24 @@ class SignUpViewController: UIViewController {
                 self.present(alertController, animated: true, completion: nil)
             
             }else{
+                let username = self.usernameTF.text ?? ""
+               var customer = userCustomer(customer: Customer(firstName: username, email: email, verifiedEmail: true, password: password, passwordConfirmation: password, sendEmailWelcome: true))
+                let customerId = self.signUpViewModel.customer.id
+                let email = Auth.auth().currentUser?.email
+                self.signUpViewModel.postCustomer(customer: customer)
                 let alertController = UIAlertController(title: "Alert", message: "Registration Sucessfully", preferredStyle: .alert)
+                
                 let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
                     let loginVc = LoginViewController(nibName: "LoginViewController", bundle: nil)
+                    loginVc.username = username
+                    loginVc.id = customerId
+                    loginVc.email = email
                     self.navigationController?.pushViewController(loginVc, animated: true)
-                }
+                    }
+                    
+                    
+                    
+                
                 
                 alertController.addAction(okAction)
                 
