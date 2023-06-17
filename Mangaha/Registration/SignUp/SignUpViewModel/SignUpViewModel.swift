3bd8b6bf921175f1f6cb1842a477a3534e80785f
Mariam Adly <mariamadly712@gmail.com>
2023-06-17 13:57:06 -0700

    add onboarding

--- Mangaha/Registration/SignUp/SignUpViewModel/SignUpViewModel.swift
+++ Mangaha/Registration/SignUp/SignUpViewModel/SignUpViewModel.swift
@@ -31,14 +31,14 @@ class SignUpViewModel{
         }
     }
     func postCart(){
-        NetworkServices.postDraftOrder { draftResponse, error in
+        NetworkServices.postDraftOrder(products: []) { draftResponse, error in
             self.cartId = draftResponse?.id ?? 0
             self.postFavorite(email: self.email , username: self.username, userId: self.id , cartId: self.cartId)
            
         }
     }
     func postFavorite(email : String , username : String , userId : Int,cartId:Int){
-        NetworkServices.postDraftOrder { draftResponse, error in
+        NetworkServices.postDraftOrder(products: []) { draftResponse, error in
             let favoriteId = draftResponse?.id
             self.db.collection("users").addDocument(data: ["id" : userId , "email" : email , "username" : username , "FavoriteId" : favoriteId , "CartId" : self.cartId])
         }
